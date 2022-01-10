d0d9dc339eac93a46ee5b8f895f8261c4f0a2bfa
Juan Vargas <juandeveloper19@gmail.com>
2022-01-10 11:57:25 -0500

    Feat: The case to handle the option to create tasks was created

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const { mainNavigation, stopExecution } = require('./helpers/inquirer');
+const { mainNavigation, stopExecution, readDataInput } = require('./helpers/inquirer');
 
 const main = async () => {
   let optionSelected = null;
 
   do {
     optionSelected = await mainNavigation();
+
+    switch (optionSelected) {
+      case 'addTask':
+        console.log('\n');
+        const rest = await readDataInput('Enter the task description:');
+
+        console.log(rest);
+        break;
+    }
 
     // Stops execution when an option is selected
     optionSelected !== 'exit' && (await stopExecution());
