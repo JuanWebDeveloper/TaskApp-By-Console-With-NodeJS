931626c60a08bbb9875370237d0ef6aa741777f0
Juan Vargas <juandeveloper19@gmail.com>
2022-01-10 16:32:34 -0500

    Feat: A getter was created that converts the object containing the task list into an array

--- models/tasks.js
+++ models/tasks.js
@@ -5,6 +5,17 @@ class Tasks {
     this.taskList = {};
   }
 
+  // Converts the task list object to an array
+  get convertListToArray() {
+    const taskListArray = [];
+
+    Object.keys(this.taskList).forEach((taskId) => {
+      taskListArray.push(this.taskList[taskId]);
+    });
+
+    return taskListArray;
+  }
+
   // Method to create a task and add to the task list
   createTask(taskDescription) {
     const task = new Task(taskDescription);
